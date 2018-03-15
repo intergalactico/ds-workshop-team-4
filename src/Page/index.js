82339332fffb9c037c7071f7aafb38337a42f3fd
katebennu <kate.dorrer@gmail.com>
2018-03-15 12:59:43 +0200

    add section component

--- src/Page/index.js
+++ src/Page/index.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import Section from '../Section/index'
 
 import * as s from './styles.js'
 
 const Page = (props) => {
   return (
-    <s.Page>Page</s.Page>
+    <s.Page>Page
+      <Section></Section>
+      <section id="navbar"></section>
+      <section id="hero"></section>
+      <section id="blue-info"></section>
+      <section id="white-keynotes"></section>
+      <section id="blue-tickets"></section>
+      <section id="blue-venue"></section>
+      <section id="black-partners"></section>
+      <section id="gray-form-feed"></section>
+      <section id="footer"></section>
+    </s.Page>
   )
 }
 
