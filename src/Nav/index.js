7e70d3e4ae354f7eae1bfdac0f23226668f82144
Don Ludwig <mail@donludwig.de>
2018-03-15 14:44:37 +0200

    Add burger menu

--- src/Nav/index.js
+++ src/Nav/index.js
@@ -1,14 +1,23 @@
 import React from 'react'
+import Icon from '../Icon';
 
 import * as s from './styles.js'
 import logo from '../../assets/dsconf-logo-small.png';
+
+import colors from '../Colors/colorset'
+
+export const Menu = Icon`
+  float: right;
+`;
+
+
 
 const Nav = (props) => {
   return (
 
     <s.Nav>
     <img src={logo} width="152" />
-
+	<Icon name="bars" color={colors.darkwhite} />
     </s.Nav>
   )
 }
