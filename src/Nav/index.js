a615b04930442379c9eb3ce007813ffc28097c53
Don Ludwig <mail@donludwig.de>
2018-03-15 14:03:30 +0200

    Add image to Navigation

--- src/Nav/index.js
+++ src/Nav/index.js
@@ -1,10 +1,15 @@
 import React from 'react'
 
 import * as s from './styles.js'
+import logo from '../../assets/dsconf-logo-small.png';
 
 const Nav = (props) => {
   return (
-    <s.Nav>Nav</s.Nav>
+
+    <s.Nav>
+    <img src={logo} width="152" />
+
+    </s.Nav>
   )
 }
 
